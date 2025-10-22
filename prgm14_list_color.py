9628735c884a742b34e00a161bedddc49857dbe4
ANJELSUBAIR <anjelsubair@gmail.com>
2025-10-22 09:54:18 +0530

    Update prgm14_list_color.py

--- prgm14_list_color.py
+++ prgm14_list_color.py
@@ -1,3 +1,3 @@
-s1=input("Enter first string: ")
-s2=input("Enter second string: ")
-print(s2[0]+s1[1:]," ",s1[0]+s2[1:])
+list1=set(['Blue','Black', 'Green','Red','Orange'])
+list2=set(['Red','Brown','Orange','Pink'])
+print(list1.difference(list2))
